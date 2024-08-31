62cad4bc36e5527a1914193f0d48bd8c15bd6c8f
Ritik Balhotra <121785590+RitikBalhotra@users.noreply.github.com>
2024-08-31 17:53:30 +0530

    first commit

---
+++ Expense Tracker/Script.js
@@ -0,0 +1,10 @@
+const expenseForm = document.getElementById('expenseForm')
+const expenseList = document.getElementById('expenseList')
+
+expenseForm.addEventListener('submit', function(event){
+    event.preventDefault();
+    
+    const description = document.getElementById('description').value;
+    const 
+    
+})
